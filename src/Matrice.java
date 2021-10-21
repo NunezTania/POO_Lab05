4e3c862fc4fc9349ae467f0489039e76d124c15f
magskwa <magali.egger@heig-vd.ch>
2021-10-21 16:25:46 +0200

    main constructeur() tostring

--- src/Matrice.java
+++ src/Matrice.java
@@ -1,17 +1,45 @@
 import java.util.Random;
+import java.util.Vector;
+
 public class Matrice {
-    int n;
-    int m;
-    //int matrice[][];
-    Matrice(int n, int m) {
+    private int n;
+    private int m;
+
+    int matrice[][];
+
+    // constructeur aléatoire
+    Matrice(int n, int m, int modulo) {
         this.n = n;
         this.m = m;
-        //matrice = new int[n][m];
+
+        int[][] mat = new int[n][m];
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                Random rand = new Random(n);
-                //matrice[i][j] = java.util.Random.Random(n);
+                Random rand = new Random(modulo);
+                mat[i][j] = 0;
             }
         }
+        matrice = mat;
+    }
+
+    // methode affichage
+    public String toString(){
+
+        // a garder ?
+        super.toString();
+
+        String result = "";
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                result += matrice[i][j];
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args){
+        Matrice m = new Matrice(3, 2, 10);
+        System.out.println(m);
     }
 }
